cd4a5ea086f8e834ee881ccfa9456b865a44b787
mywat88 <viktorsink202@gmail.com>
2021-05-11 14:01:50 +0300

    Slider was realized

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import data from '../images.json'
 
+
+// generate images 
+function* image_generator()
+{
+    while(true)
+    {
+        for(const [,url] of Object.entries(data))
+        {
+            yield url;
+        }
+    }
+}
 
 
 class Slidebar extends React.Component
 {
 
+    slider_time = 5000;
+    constructor(props)
+    {
+        super(props);
+        this.generator = image_generator();
+        this.state = {image:this.generator.next().value};
 
+    }
 
+    nextImage()
+    {
+        this.setState({
+            image: this.generator.next().value
+        });
+    }
+
+    componentDidMount()
+    {
+        this.timer = setInterval(
+            () => this.nextImage(),
+            this.slider_time
+        );
+    }
+
+    componentWillUnmount() 
+    {
+        clearInterval(this.timer);
+    }
 
     render()
-    {
+    {  
+
+        const generator = image_generator();
 
         return(
-            <div className='slider' >
-            <img src='https://media.dertouristik.com/exports/KuoniUK/ImageGalleryLightboxLarge/486274_ImageGalleryLightboxLarge.jpg'></img>
+            <div className='slider' 
+            style={{backgroundImage: `url(${this.state.image})`}}
+            >
             </div>
         );
     }
