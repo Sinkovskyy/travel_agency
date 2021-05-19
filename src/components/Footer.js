e05dd41f0f2d4219ba80c338ec375c0bc16e3f37
mywat88 <viktorsink202@gmail.com>
2021-05-19 18:45:54 +0300

    Click event on footer's items was added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,21 @@ function Footer()
 
     const ICON_SIZE = 30;
 
+
+    // Click on items handler
+    function clickHandler(event)
+    {
+        // Get value of current clicked item
+        const data = event.target.firstChild.data;
+        // Find class of pressed element and move to the section
+        Object.keys(NavItemsData).forEach(i => {
+            if(NavItemsData[i].name == data)
+            {
+                document.getElementsByClassName(NavItemsData[i].position)[0].scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
+
     return(
      <div className='footer'>
             <div className='logo'>FAST TRAVEL</div>
@@ -20,7 +35,7 @@ function Footer()
                 <div className="container">
             {Object.keys(NavItemsData).map((item) =>
             {
-                return <a className="nav-item" key={item}>{NavItemsData[item].name}</a>
+                return <a className="nav-item" onClick={clickHandler} key={item}>{NavItemsData[item].name}</a>
             })}
                 </div>
             </div>
