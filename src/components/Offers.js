a001c3a976888a7fab8c67e87a947d0196633963
mywat88 <viktorsink202@gmail.com>
2021-05-21 20:24:02 +0300

    Offers section are ready

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,13 +1,80 @@
 import ApartmentsData from "../data/offers.json";
+import {AiOutlineLeft as Left,AiOutlineRight as Right} from "react-icons/ai";
+import {useState} from "react";
 
+
+
+function InfoBar(props)
+{
+
+    const bars = [];
+
+    for(let i = 0; i < props.length;i++)
+    {
+        const className = (props.activePosition == i) ? "active bar":"bar"; 
+        bars[i] = <div class={className}></div>
+    }
+
+    return(
+        <div className={props.className}>
+          {bars.map((item) => item)}
+        </div>
+    );
+}
+
+
+
+
+
+function OfferFrame(props)
+{
+
+
+    const [imageIndex,setImageIndex] = useState(0);
+
+    function leftClickHandler()
+    {
+        if(imageIndex == 0)
+            setImageIndex(props.images.length - 1);
+        else
+            setImageIndex(imageIndex - 1);
+    }
+
+    function rightClickHandler()
+    {
+        if(imageIndex + 1 == props.images.length)
+            setImageIndex(0);
+        else
+            setImageIndex(imageIndex + 1);
+    }
+
+
+
+    return(
+        <div className="offer-frame"> 
+            <div className="image-container">
+                <img src={props.images[imageIndex]}/>
+                <div className="layer"></div>
+                <button className="book">Book now</button>
+                <button className="slide left" onClick={leftClickHandler}><Left className="icon" /></button>
+                <button className="slide right" onClick={rightClickHandler}><Right className="icon" /></button>
+                <InfoBar className="slider-infobar" length={props.images.length} activePosition={imageIndex} />
+            </div>
+            <h3>{props.title}</h3>
+            <h4>${props.price }\PER NIGHT</h4>
+        </div>
+    );
+
+}
 
 
 function Offers()
 {
     
+
     return(
-        <div className="offers">
-            <h2>Hotels & Houses</h2>
+        <div className="offers" >
+            <h2>Hotels & Apartments</h2>
             <p>On Bali there are so many variety of dwelling for everyone tastes.
              So if you want something exsotic be sure 
              that on Bali you will find what you are looking!</p>
@@ -15,15 +82,10 @@ function Offers()
                 {Object.keys(ApartmentsData).map(i => 
                 {
                     return(
-                        <div className="offer-frame" key={i}> 
-                            <div className="image-container">
-                                <img src={ApartmentsData[i].image}/>
-                                <div className="layer"></div>
-                                <button>Book now</button>
-                            </div>
-                            <h3>{ApartmentsData[i].title}</h3>
-                            <h4>${ApartmentsData[i].price }\PER NIGHT</h4>
-                        </div>
+                        <OfferFrame  title={ApartmentsData[i].title}
+                             price={ApartmentsData[i].price} 
+                             images={ApartmentsData[i].images} />
+                        
                         );
                 })}
             </div>
